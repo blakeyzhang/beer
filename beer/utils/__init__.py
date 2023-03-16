ba45f6aa0cf035adea8c9c01bdb0d9362517793f
blakeyzhang <zhangyiming07@163.com>
2023-03-16 07:27:39 +0000

    add constants

---
+++ beer/utils/__init__.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py
+@Time    :   2023/03/16 15:25:38
+@Author  :   blakeyzhang
+@Version :   1.0
+@Desc    :   None
+'''
---
+++ beer/utils/constants.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   constants.py
+@Time    :   2023/03/16 15:26:26
+@Author  :   blakeyzhang
+@Version :   1.0
+@Desc    :   None
+'''
+
+ALL_HTTP_METHODS = ('GET', 'POST', 'HEAD', 'PUT', 'DELETE', 'OPTIONS', 'PATCH')
